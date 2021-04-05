30167b2235c2ce4e08d685039af78c649870e191
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2021-04-05 20:50:50 +0200

    Add files via upload

--- FlappyBirb.py
+++ FlappyBirb.py
@@ -15,12 +15,12 @@ pipe_surface = pygame.image.load('pipe.png').convert_alpha()
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
-pipe_height = [400, 600, 800]
+pipe_height = [400, 500, 600, 650, 700, 750, 800, 850]
 #valtozok
 fl_xpos = 0
 gravity = 0.25
 bird_movement = 0
-
+game_active = True
 
 
 #fuggvenyek
@@ -32,8 +32,9 @@ def draw_floor():
 
 def create_pipe():
     random_pipe_pos = random.choice(pipe_height)
-    new_pipe = pipe_surface.get_rect(midtop = (700,512))
-    return new_pipe
+    bottom_pipe = pipe_surface.get_rect(midtop = (700,random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom = (700,random_pipe_pos - 300))
+    return bottom_pipe, top_pipe
 
 
 def move_pipes(pipes):
@@ -47,6 +48,18 @@ def draw_pipes(pipes):
     for pipe in pipes:
         screen.blit(pipe_surface, pipe)
     
+    
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+
+            return False
+
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        return False
+
+    return True
+           
 
 #gameloop
 while True:
@@ -55,23 +68,31 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 10
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100, 512)
+                bird_movement = 0
+                
         if event.type == SPAWNPIPE:
-            pipe_list.append(create_pipe())
+            pipe_list.extend (create_pipe())
     
     screen.blit(bg_surface, (0,0))
-    screen.blit(bird_surface, bird_rect)
     
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    
-    
-    
-    #pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+    if game_active:
+        #birb
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface, bird_rect)
+        game_active = check_collision(pipe_list)
+        
+        
+        #pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
     
     
     
@@ -80,7 +101,7 @@ while True:
     
     
     #floor 
-    fl_xpos -=1
+    fl_xpos -=5
     draw_floor()
     if fl_xpos <= -576:
         fl_xpos = 0
